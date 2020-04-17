9b5ec3ed21f85d45c87ad31b2ff8c4ae54bbf432
Burak Tilek <burak.tilek@gmail.com>
2020-04-18 02:52:42 +0300

    GET drinks-detail endpoint is implemented

--- backend/src/api.py
+++ backend/src/api.py
@@ -14,14 +14,6 @@ CORS(app)
 db_drop_and_create_all()
 
 ## ROUTES
-'''
-@TODO implement endpoint
-    GET /drinks
-        it should be a public endpoint
-        it should contain only the drink.short() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 @app.route('/drinks')
 def get_drinks():
   drinks = Drink.query.order_by(Drink.id).all()
@@ -33,14 +25,16 @@ def get_drinks():
   })
 
 
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks-detail')
+@requires_auth('get:drinks-detail')
+def get_drink_details(payload):
+  drinks = Drink.query.order_by(Drink.id).all()
+  drinks_long = [drink.long() for drink in drinks]
+
+  return jsonify({
+    "success": True,
+    "drinks": drinks_long,
+  })
 
 
 '''
