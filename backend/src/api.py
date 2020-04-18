a102a62a48dc9f04b7021c622e082bab860194a7
Burak Tilek <burak.tilek@gmail.com>
2020-04-18 03:12:01 +0300

    POST /drinks endpoint is implemented

--- backend/src/api.py
+++ backend/src/api.py
@@ -37,15 +37,24 @@ def get_drink_details(payload):
   })
 
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks', methods=['POST'])
+@requires_auth('post:drinks')
+def create_drink(payload):
+  body = request.get_json()
+  try:
+    drink = Drink(
+      title = body.get('title', None),
+      recipe = json.dumps(body.get('recipe', None)),
+    )
+    drink.insert()
+
+    return jsonify({
+      "success": True,
+      "drinks": [drink.long()],
+    })
+
+  except:
+    abort(422)
 
 
 '''
