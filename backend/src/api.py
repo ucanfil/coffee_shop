90e19a2fc5af78dac73e33f7dd3d8a052c862ccc
Burak Tilek <burak.tilek@gmail.com>
2020-04-18 03:24:10 +0300

    PATCH /drinks/<int:id> endpoint is implemented

--- backend/src/api.py
+++ backend/src/api.py
@@ -57,17 +57,26 @@ def create_drink(payload):
     abort(422)
 
 
-'''
-@TODO implement endpoint
-    PATCH /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should update the corresponding row for <id>
-        it should require the 'patch:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
-        or appropriate status code indicating reason for failure
-'''
+@app.route('/drinks/<int:id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def update_drink(payload, id):
+  if id is None:
+    abort(404)
+
+  body = request.get_json()
+
+  try:
+    drink = Drink.query.filter(Drink.id == id).one_or_none()
+    drink.title = body.get('title', None)
+    drink.recipe = json.dumps(body.get('recipe', None))
+    drink.update()
+
+    return jsonify({
+      "success": True,
+      "drinks": [drink.long()]
+    })
+  except:
+    abort(404)
 
 
 '''
